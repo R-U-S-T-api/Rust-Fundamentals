e1ed5b46a082534a7d305272203d76aea80e0acd
john patrick batino <rust.api@outlook.com>
2026-02-02 19:34:04 +0800

    Create generic_data.rs

---
+++ 06_traits/generic_data.rs
@@ -0,0 +1,32 @@
+#![allow(unused)]
+
+// option results vec
+
+enum Option<T> {
+    Some(T),
+    None
+}
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E)
+}
+
+struct Point<T = u32> {
+    x: T,
+    y: T,
+}
+
+
+fn main() {
+
+    let x: Option<u32> = Option::Some(1);
+    let x: Option<i32> = Option::Some(-1);
+
+    let res: Result<bool, String> = Result::Ok(true);
+
+    let v: Vec<_> = vec![1,2,3];
+
+    let p0: Point<i32> = Point{x: -1, y: 9};
+
+}
